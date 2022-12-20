b61935ecdf1ea96cc73fd4d90da99bbf57d29290
David Alliband <cr21246@bristol.ac.uk>
2022-12-20 10:54:07 +0000

    Calculator script creates dataframe with all calculations working correctly.

--- western_dilution_calculator.py
+++ western_dilution_calculator.py
@@ -3,25 +3,47 @@ import math
 import pandas as pd
 
 # Request CSV filename for use in calculations
-TP_file = input("What is your CSV filename? ")
+TP_file = input("Enter your CSV filepath: ")
 
 # Read in CSV and print
 TP_data = pd.read_csv(f"{TP_file}")
 print(TP_data)
 
-# Calculate sample volumes for 20ug of protein and add to dataframe
-TP_data["Sample_volume_ul"] = ([20000] / TP_data["Total_protein"])
+# Request original volume of total protein
+TP_data["Total_protein_volume"] = int(input("What is your total protein volume in ul? (e.g. for protein per ml enter 1000 or for per 500ul enter 500) "))
+
+# Request dilution factor for use in calculations
+TP_data["Dilution_factor"] = int(input("What is your dilution factor? (e.g. if calculating from protein per ml then use 20000 for 20ug of protein or 15000 for 15ug of protein) "))
+
+# Calculate sample volumes for desired amount of protein and add to dataframe
+TP_data["Sample_volume_ul"] = (TP_data["Dilution_factor"] / TP_data["Total_protein"])
 print(TP_data)
 
-# Calculate buffer volumes based on highest sample volume and add to dataframe
-TP_data["Buffer_volume_ul"] = ([20000] / TP_data["Total_protein"])
+# Calculate ug of protein and add to dataframe
+TP_data["Sample_protein_quantity"] = (TP_data["Dilution_factor"] / TP_data["Total_protein_volume"])
 print(TP_data)
+
+# Request total solution quantity required
+TP_data["Total_solution_ul"] = int(input("What is your desired total volume in ul? "))
+
+# Request loading dye fraction required
+TP_data["Loading_dye_fraction"] = int(input("What is your desired loading dye fraction? (e.g. for a 1 in 5 dilution enter 5) "))
 
 # Calculate loading dye volume and add to dataframe
-TP_data["Buffer_volume_ul"] = ([20000] / TP_data["Total_protein"])
+TP_data["Loading_dye_volume_ul"] = (TP_data["Total_solution_ul"] / TP_data["Loading_dye_fraction"])
 print(TP_data)
 
-# dilutions = list(dilutionsList)
-# dilutionsDF = pd.DataFrame.from_dict(dilutions)
-# display(dilutionsDF)
-# dilutionsDF.to_csv("Export.csv")
+# Calculate buffer volumes based on total desired volume minus sample and loading dye volumes and add to dataframe
+TP_data["Buffer_volume_ul"] = (TP_data["Total_solution_ul"] - (TP_data["Loading_dye_volume_ul"] + TP_data["Sample_volume_ul"]))
+print(TP_data)
+
+# Tidy up dataframe by reordering columns
+TP_data = TP_data.reindex(columns=["Content", "Total_protein", "Dilution_factor", "Sample_protein_quantity", "Loading_dye_fraction", "Total_solution_ul", "Sample_volume_ul", "Buffer_volume_ul", "Loading_dye_volume_ul"])
+
+# Tidy up dataframe by renaming columns
+TP_data.set_axis(["Content", "Total protein", "Dilution factor", "Sample protein quantity", "Loading dye fraction", "Total solution", "Sample volume", "Buffer volume", "Loading dye volume"], axis='columns', inplace=True)
+
+# Tidy up dataframe by adding value measurements
+# TP_data = TP_data.reindex(columns=["Content", "Total_protein", "Dilution_factor", "Sample_protein_quantity", "Loading_dye_fraction", "Total_solution_ul", "Sample_volume_ul", "Buffer_volume_ul", "Loading_dye_volume_ul"])
+
+TP_data.to_csv("Export.csv")
